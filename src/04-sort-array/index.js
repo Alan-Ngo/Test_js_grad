101f1c3a834b4e4831af39d390c5405afd8e4b14
Alan <alan99179@gmail.com>
2021-07-15 21:25:50 +0100

    04-sort-array finished

--- src/04-sort-array/index.js
+++ src/04-sort-array/index.js
@@ -7,9 +7,34 @@
  *   - Not required, but major bonus points awarded
  */
 
-// Average and worst case time complexity (Big-O) of my function is:
-// Explain:
+// Average and worst case time complexity (Big-O) of my function is: O(n^2)
+// Explain: Bubble sort used to sort array, requires two for loops
 
 module.exports = function sortArray(arr) {
-  // TODO
+  if(arr.length == 0){
+    return []
+  }
+
+  if(!Array.isArray(arr)){
+    throw new TypeError("Not a number")
+  }else{
+    for(var i = 0; i < arr.length; i++){
+      if(typeof arr[i] != "number" || !Number.isInteger(arr[i])){
+        console.log("thwewewe")
+        throw new TypeError("Not a number2")
+      }    
+    }
+  }
+
+  for(var i = 0; i < arr.length; i++){
+    for(var j = 0; j < arr.length-1; j++){
+      if(arr[j]>arr[j+1]){
+        temp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = temp
+      }
+    }
+  }
+
+  return arr
 };
