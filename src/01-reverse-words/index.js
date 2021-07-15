2f6e94eb3746955e4c374d923372ccc20b5b5213
Alan <alan99179@gmail.com>
2021-07-15 01:39:46 +0100

    Completed reverse words

--- src/01-reverse-words/index.js
+++ src/01-reverse-words/index.js
@@ -4,9 +4,21 @@
  *   - contains words that are divided by a single space
  */
 
-// Average and worst case time complexity (Big-O) of my function is:
-// Explain:
+// Average and worst case time complexity (Big-O) of my function is: O(n)
+// Explain: 
+// O(n) for split, reverse and join behind the scenes
 
 module.exports = function reverseWordsInSentence(input) {
-  // TODO
+  var input = input.split(" ");
+
+  var reversedString = ""
+
+  for(var i = 0; i<input.length; i++){
+    reversedString += input[i].split("").reverse().join("");
+    if(i!=input.length-1){
+      reversedString += " "
+    }
+  }
+
+  return reversedString
 };
