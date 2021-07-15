301986c321861eb4b727673fbffedd0e4157cbda
Alan <alan99179@gmail.com>
2021-07-15 19:59:32 +0100

    Completed 02-create-base

--- src/02-create-base/index.js
+++ src/02-create-base/index.js
@@ -1,8 +1,13 @@
 // You can assume the input is an integer
 
-// Average and worst case time complexity (Big-O) of my function is:
+// Average and worst case time complexity (Big-O) of my function is: O(1)
 // Explain:
+//Only addition is used which is O(1) to calculate the addition of numbers
 
 module.exports = function createBase(integer) {
-  // TODO
+  function add(v){
+    return integer + v;
+  }
+
+  return add;
 };
